fe5b5b097fc85c1c0b2bd0b390c6f72eaff42dec
Sanjana Natraj <sanjana.natraj319@gmail.com>
2021-03-12 17:09:01 -0800

    added api key

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,9 +1,23 @@
+const dotenv = require('dotenv')
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+const url = 'https://api.meaningcloud.com/sentiment-2.1'
+const key = process.env.API_KEY
+
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 const app = express()
-
+app.use(cors())
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(express.static('dist'))
 
 console.log(__dirname)
